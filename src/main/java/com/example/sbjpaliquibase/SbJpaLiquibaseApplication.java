70ead86ac6b5cf14d831ac3129b00cf522f30c10
Daniele <daniele.ninicato@gmail.com>
2023-12-31 15:09:46 +0100

    Initial commit

---
+++ src/main/java/com/example/sbjpaliquibase/SbJpaLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbjpaliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJpaLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJpaLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbjpaliquibase/SbJpaLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbjpaliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJpaLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
